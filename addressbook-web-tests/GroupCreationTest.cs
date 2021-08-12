aa6696fc2d408474ab641a721721ffe039621b0a
ramzia <ramzia9921@gmail.com>
2021-08-12 17:13:09 +0300

    Refactoring GroupCreationTest. Created a helper class to store contact group properties, initialize test data and to pass that data to helper methods(issue 3)

--- addressbook-web-tests/GroupCreationTest.cs
+++ addressbook-web-tests/GroupCreationTest.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace addressbook_web_tests
-{
+namespace WebAddressbookTests
+{ 
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTest
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -39,31 +39,69 @@ namespace addressbook_web_tests
             Assert.AreEqual("", verificationErrors.ToString());
         }
 
+
         [Test]
         public void TheUntitledTestCaseTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("a");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("a");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("a");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
+            OpenHomePage();
+            LogIn(new AccountData("admin","secret"));
+            GoToGroupPage();
+            InitGroupCreation();
+            FillGroupCreation(new GroupData("qweName", "qweHeader", "qweFooter"));
+            SubmitGrpupPage();
+            BackToGroupPage();
+            LogOut();
+        }
+
+        private void LogOut()
+        {
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void BackToGroupPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void SubmitGrpupPage()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+        private void FillGroupCreation(GroupData groupdata)
+        {
+            FillField("group_name", groupdata.Name);
+            FillField("group_header", groupdata.Header);
+            FillField("group_footer", groupdata.Footer);
+        }
+
+        private void FillField(string name_of_element, string value)
+        {
+            driver.FindElement(By.Name(name_of_element)).Click();
+            driver.FindElement(By.Name(name_of_element)).Clear();
+            driver.FindElement(By.Name(name_of_element)).SendKeys(value);
+        }
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void LogIn(AccountData accountdata)
+        {
+            FillField("user", accountdata.Username);
+            FillField("pass", accountdata.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
